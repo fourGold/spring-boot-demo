d63b214d004bc07993e942f499118a5620194c36
songrgg <songrgg0.0@gmail.com>
2016-03-05 03:56:17 +0800

    Configure the swagger to the RestFul API.

---
+++ src/main/java/com/wallstreetcn/Application.java
@@ -0,0 +1,82 @@
+package com.wallstreetcn;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("wallstreetcn")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST Sample with Swagger")
+                .description("Spring REST Sample with Swagger")
+                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
+                .version("2.0")
+                .build();
+    }
+
+//    @Bean
+//    public CommandLineRunner demo(CustomerRepository repository) {
+//        return (args) -> {
+//            // save a couple of customers
+//            repository.save(new Customer("Jack", "Bauer"));
+//            repository.save(new Customer("Chloe", "O'Brian"));
+//            repository.save(new Customer("Kim", "Bauer"));
+//            repository.save(new Customer("David", "Palmer"));
+//            repository.save(new Customer("Michelle", "Dessler"));
+//
+//            // fetch all customers
+//            log.info("Customers found with findAll():");
+//            log.info("-------------------------------");
+//            for (Customer customer : repository.findAll()) {
+//                log.info(customer.toString());
+//            }
+//            log.info("");
+//
+//            // fetch an individual customer by ID
+//            Customer customer = repository.findOne(1L);
+//            log.info("Customer found with findOne(1L):");
+//            log.info("--------------------------------");
+//            log.info(customer.toString());
+//            log.info("");
+//
+//            // fetch customers by last name
+//            log.info("Customer found with findByLastName('Bauer'):");
+//            log.info("--------------------------------------------");
+//            for (Customer bauer : repository.findByLastName("Bauer")) {
+//                log.info(bauer.toString());
+//            }
+//            log.info("");
+//        };
+//    }
+
+}
